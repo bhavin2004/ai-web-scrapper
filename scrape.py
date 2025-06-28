075aadb4b9ac6d4492a8a44f488a7ffd7e965d42
Bhavin Karangia <omkarpro6@gmail.com>
2025-06-28 14:03:12 +0530

    removed uc chrome

--- scrape.py
+++ scrape.py
@@ -4,47 +4,44 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 def scrape_website(website_url):
-    # service= Service(executable_path='chromedriver')  
     options = webdriver.ChromeOptions()
     options.add_argument('--disable-gpu')
-    options.add_argument('--headless') 
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage') 
-    options.add_argument('--window-size=1920,1080')  # Set a default window size
-    options.add_argument('--ignore-certificate-errors')  # Ignore SSL errors
-    options.add_argument('--allow-running-insecure-content')  # Allow insecure content
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--allow-running-insecure-content')
+
+    # Corrected line
+    options.binary_location = "/usr/bin/chromium"
+
+    # You are using ChromeDriverManager here instead of the fixed path
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
+
     try:
         driver.get(website_url)
-        driver.save_screenshot('screenshot.png')  
+        driver.save_screenshot('screenshot.png')
         page_source = driver.page_source
         return page_source
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
     finally:
-        driver.quit()   
-        
+        driver.quit()
+
 def extract_body_content(page_source):
     soup = BeautifulSoup(page_source, 'html.parser')
     body_content = soup.find('body')
-    if body_content:
-        return str(body_content)
-    return ""
+    return str(body_content) if body_content else ""
 
 def clean_body_content(body_content):
     soup = BeautifulSoup(body_content, 'html.parser')
-    
-    # Remove script and style elements
-    for script_or_style in soup(['script', 'style']):   
-        script_or_style.decompose()
-    clean_content = soup.get_text(separator='\n', strip=True).splitlines()
-    # print("Cleaned content length:", len(clean_content))
-    clean_content='\n'.join(content.strip() for content in clean_content if content.strip())
-    return clean_content
+    for tag in soup(['script', 'style']):
+        tag.decompose()
+    lines = soup.get_text(separator='\n', strip=True).splitlines()
+    return '\n'.join(line.strip() for line in lines if line.strip())
 
 def split_dom_content(dom_content, max_length=6000):
-    return[
-        dom_content[i:i + max_length] for i in range(0, len(dom_content), max_length)
-    ]
+    return [dom_content[i:i + max_length] for i in range(0, len(dom_content), max_length)]
